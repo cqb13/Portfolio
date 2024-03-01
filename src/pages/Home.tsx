72a48b3c412ca347f9f9c28a8885bd9674f1d156
cqb13 <cqb13.main@gmail.com>
2024-03-01 18:13:23 -0500

    update main name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,9 @@ const Home = () => {
       <img src={logo} alt='logo' className='large-logo zoom-in' />
       <section className='name-display'>
         <h1 className='zoom-in'>Hi, i'm </h1>
-        <h1 className='accented-text zoom-in'>cqb13</h1>
+        <h1 className='accented-text zoom-in'>Maksim Straus</h1>
       </section>
-      <sub className="zoom-in">Web-Developer</sub>
+      <sub className='zoom-in'>Web-Developer</sub>
       <NavBar type='homepage' />
     </main>
   );
