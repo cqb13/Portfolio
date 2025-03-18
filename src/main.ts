a732087767443ad75d1496c2b14007895f903da6
cqb13 <cqb13.main@gmail.com>
2025-03-17 22:30:11 -0400

    removed router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
-import './style.css'
-
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
